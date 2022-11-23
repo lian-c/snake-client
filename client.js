b283b5daecdd5701a93ae9cce47e0309564a4ed2
lian-c <lian.chan@telus.com>
2022-11-23 14:04:46 -0800

    Added ctrl -c key to close game

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require("net");
+// const {setupInput} = require('./play.js')
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
--- play.js
+++ play.js
@@ -2,3 +2,25 @@ const {connect} = require('./client.js')
 
 console.log("Connecting ...");
 connect();
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput); //event listener
+  return stdin;
+};
+
+const handleUserInput = function (key){
+
+  if (key === '\u0003') {
+    console.log("You wanna quit??")
+    process.exit();
+  }
+}
+
+setupInput();
+
+// module.exports = {setupInput}
